86b31b854d4f341e4abfd8f80ebe341f80250e01
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2021-05-25 23:32:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Python Algorithms and Data Structures
+
+### Summary
+With this respository, I am solving Algorithms in Python Version 3.8.2 with notes including Big O time complexity analyisis.
+
+## Core Concepts
+- Iterative vs Recursive
+- Variables/Pointers Manipulation
+- Binary Search
+- Hashmap
+- Stack
+- Array
+- Linked List
+- Sorting
+- Depth First Search
+- Breadth First Search
+- Memoization
---
+++ algorithms/reverseArray.py
@@ -0,0 +1,20 @@
+import math
+# reverse array
+
+def reverseArray(array):
+    # loop up until the midpoint in array for odd numbers math.floor array length / 2
+    length = len(array)
+    mid = math.floor(length/2)
+
+    for i in range(mid):
+        # create a temp var to hold value
+        temp = array[i]
+        # length 3
+        # index 3 - 1 - 0 which is 2
+        array[i] = array[length - 1 - i] # assign first index of array to last item in array
+        array[length - 1 - i] = temp
+
+    return array
+
+result = reverseArray([1, 2, 3, 4, 5])
+print(result)
